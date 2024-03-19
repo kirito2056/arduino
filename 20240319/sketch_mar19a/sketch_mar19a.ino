cbe07009e3ca197fef498da9b24d491f50199e01
하진 권씨 <hajin_2056@naver.com>
2024-03-19 22:03:41 +0900

    20240319

---
+++ 20240319/sketch_mar19a/sketch_mar19a.ino
@@ -0,0 +1,14 @@
+#define LEDPIN 4
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LEDPIN, HIGH);
+  delay(100);
+  digitalWrite(LEDPIN ,LOW);
+  delay(100);
+}
